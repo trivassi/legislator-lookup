e02d66dce6840eedc5eb4d80e9e5e2e728d20400
Thomas Rivas and na <thomas+na@>
2016-11-01 11:04:06 -0700

    succesfully search legislators when zipcode is entered

---
+++ app/routes/results.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+import config from '../config/environment';
+
+
+export default Ember.Route.extend({
+  model: function(params) {
+    var key = config.myApiKey;
+    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=' + key + '&zip=' + params.zip;
+    return Ember.$.getJSON(url).then(function(responseJSON) {
+      return responseJSON.results;
+    });
+  }
+});
